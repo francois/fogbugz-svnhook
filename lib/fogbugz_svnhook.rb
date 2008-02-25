b9d289a0d5bd7014b837f6ec7ac3d7ed76ba9a91
fbos <fbos@de84c797-fc5b-4eda-bf1c-de7fe2d7ab48>
2008-02-25 03:44:14 +0000

    Initial stub implementation of commit subcommand.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/fogbugz-svnhook/trunk@15 de84c797-fc5b-4eda-bf1c-de7fe2d7ab48

--- lib/fogbugz_svnhook.rb
+++ lib/fogbugz_svnhook.rb
@@ -2,6 +2,7 @@ $:.unshift File.dirname(__FILE__)
 
 require "fogbugz_svnhook/login"
 require "fogbugz_svnhook/logoff"
+require "fogbugz_svnhook/commit"
 
 module FogbugzSvnhook
   class << self
@@ -11,6 +12,10 @@ module FogbugzSvnhook
 
     def logoff(options)
       FogbugzSvnhook::Logoff.new(options).run
+    end
+
+    def commit(options)
+      FogbugzSvnhook::Commit.new(options).run
     end
   end 
 end
---
+++ lib/fogbugz_svnhook/commit.rb
@@ -0,0 +1,50 @@
+require "fogbugz_svnhook/base"
+
+module FogbugzSvnhook
+  class Commit < FogbugzSvnhook::Base
+    attr_reader :svnlook, :repository, :revision
+
+    def initialize(options={})
+      super
+      @svnlook = options[:svnlook] || config[:svnlook] || "/usr/bin/svnlook"
+      @repository = options[:repository] || config[:repository]
+      @revision = Integer(options[:revision])
+    end
+
+    def run
+      get_missing_options
+      msg = get_commit_message
+      cases_to_manage = parse(msg)
+      committer = map_committer_to_token
+      update_cases(committer, cases_to_manage)
+    end
+
+    def config
+      {}
+    end
+
+    def get_missing_options
+      say "Get missing options"
+    end
+
+    def get_commit_message
+      say "Retrieving commit message"
+      msg = `#{svnlook} log --revision #{revision} #{repository}`
+      return msg if $?.success?
+      raise "Failed to get commit message, svnlook exited with status #{$?.exitstatus}"
+    end
+
+    def parse(msg)
+      say "Parsing #{msg.inspect}"
+    end
+
+    def map_committer_to_token
+      say "Mapping committer to token"
+    end
+
+    def update_cases(committer, cases)
+      connect
+      say "Updating cases"
+    end
+  end
+end
