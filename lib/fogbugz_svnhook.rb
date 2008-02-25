8265967600c1c951dd403508d097d1dec96bbf2a
fbos <fbos@de84c797-fc5b-4eda-bf1c-de7fe2d7ab48>
2008-02-25 03:44:12 +0000

    Use HighLine to query and talk to the user.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/fogbugz-svnhook/trunk@11 de84c797-fc5b-4eda-bf1c-de7fe2d7ab48

--- lib/fogbugz_svnhook.rb
+++ lib/fogbugz_svnhook.rb
@@ -6,37 +6,37 @@ require "open-uri"
 require "rexml/document"
 require "rexml/xpath"
 require "active_support"
+require "highline"
 
 module FogbugzSvnhook
   class << self
     def login(options)
+      @hl = HighLine.new
       @url = options[:url]
       @email = options[:email]
       @password = options[:password]
 
       if @email.nil? || @email.strip.empty? then
-        print "Type the E-Mail address you use on FogBugz: "
-        @email = gets.chomp
+        @email = @hl.ask("Type the E-Mail address you use on FogBugz: ")
       end
 
       if @password.nil? || @password.strip.empty? then
-        print "Type the password you use on FogBugz: "
-        @password = gets.chomp
+        @password = @hl.ask("Type the password you use on FogBugz: ") {|q| q.echo = false}
       end
 
       api_uri = @url.merge("api.xml")
-      puts "Connecting to #{api_uri}..."
+      @hl.say "Connecting to #{api_uri}..."
       doc = REXML::Document.new(open(api_uri))
       api_url = REXML::XPath.first(doc.root, "//response/url/text()")
       api_uri = api_uri.merge(api_url.to_s)
-      puts api_uri
+      @hl.say api_uri
 
       login_uri = api_uri.dup
       login_uri.query = {"cmd" => "logon", "email" => @email, "password" => @password}.to_query
-      puts "Login on to #{login_uri}"
+      @hl.say "Login on to #{login_uri}"
       doc = REXML::Document.new(open(login_uri))
       token = REXML::XPath.first(doc.root, "//response/token/text()")
-      puts "Your logon token:\n#{token}"
+      @hl.say "Your logon token:\n#{token}"
     end
   end 
 end
