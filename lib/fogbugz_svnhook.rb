3414aea8fa7c3721d8494493f9a4ff8bf40d028f
fbos <fbos@de84c797-fc5b-4eda-bf1c-de7fe2d7ab48>
2008-02-25 01:23:14 +0000

    Begin implementing login sub-command.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/fogbugz-svnhook/trunk@4 de84c797-fc5b-4eda-bf1c-de7fe2d7ab48

--- lib/fogbugz_svnhook.rb
+++ lib/fogbugz_svnhook.rb
@@ -1,5 +1,29 @@
 $:.unshift File.dirname(__FILE__)
 
+require "net/http"
+require "net/https"
+require "open-uri"
+
 module FogbugzSvnhook
-  
+  class << self
+    def login(options)
+      @url = options[:url]
+      @email = options[:email]
+      @password = options[:password]
+
+      if @email.nil? || @email.strip.empty? then
+        print "Type the E-Mail address you use on FogBugz: "
+        @email = gets.chomp
+      end
+
+      if @password.nil? || @password.strip.empty? then
+        print "Type the password you use on FogBugz: "
+        @password = gets.chomp
+      end
+
+      api_url = @url.merge("api.xml")
+      puts "Connecting to #{api_url}..."
+      puts open(api_url).string
+    end
+  end 
 end
