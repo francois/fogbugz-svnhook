fb7d8396c4aac396d8df5b0cd26d6150cff48267
fbos <fbos@de84c797-fc5b-4eda-bf1c-de7fe2d7ab48>
2008-02-25 03:44:17 +0000

    Implemented reopen/reactivate, but not reassignment: cases stay assigned to CLOSED.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/fogbugz-svnhook/trunk@23 de84c797-fc5b-4eda-bf1c-de7fe2d7ab48

--- lib/fogbugz_svnhook/commit.rb
+++ lib/fogbugz_svnhook/commit.rb
@@ -41,7 +41,7 @@ module FogbugzSvnhook
 
     def parse(msg)
       returning(Hash.new {|h,k| h[k] = Array.new}) do |actions|
-        msg.scan(/(?:(?:implements?)|(?:fix(?:es)?)|(?:closes?)):?\s*#\d+(?:,\s*#\d+)*/i) do |section|
+        msg.scan(/(?:(?:(?:re)?(?:opens?|activates?))|(?:implements?)|(?:fix(?:es)?)|(?:closes?)):?\s*#\d+(?:,\s*#\d+)*/i) do |section|
           case section
           when /close/i
             section.scan(/#\d+/) do |bugid|
@@ -54,6 +54,14 @@ module FogbugzSvnhook
           when /implement/i
             section.scan(/#\d+/) do |bugid|
               actions[bugid[1..-1].to_i] << :implement
+            end
+          when /activate/i
+            section.scan(/#\d+/) do |bugid|
+              actions[bugid[1..-1].to_i] << :reactivate
+            end
+          when /open/i
+            section.scan(/#\d+/) do |bugid|
+              actions[bugid[1..-1].to_i] << :reopen
             end
           else
             raise "Unhandled section: #{section.inspect}"
@@ -107,8 +115,30 @@ module FogbugzSvnhook
                           "sEvent" => "Implemented in r#{revision}."}.to_query
       doc = read(action_uri)
       error = REXML::XPath.first(doc.root, "//response/error")
-      raise "Could not fix #{bugid}:\n#{doc}" if error
+      raise "Could not implement #{bugid}:\n#{doc}" if error
       $stderr.puts "Implemented #{bugid}"
+    end
+
+    def reopen(bugid, committer)
+      action_uri = api_uri.dup
+      action_uri.query = {"cmd" => "reopen", "token" => committer,
+                          "ixBug" => bugid,
+                          "sEvent" => "Reopened in r#{revision}."}.to_query
+      doc = read(action_uri)
+      error = REXML::XPath.first(doc.root, "//response/error")
+      raise "Could not reopen #{bugid}:\n#{doc}" if error
+      $stderr.puts "Reopened #{bugid}"
+    end
+
+    def reactivate(bugid, committer)
+      action_uri = api_uri.dup
+      action_uri.query = {"cmd" => "reactivate", "token" => committer,
+                          "ixBug" => bugid,
+                          "sEvent" => "Reactivated in r#{revision}."}.to_query
+      doc = read(action_uri)
+      error = REXML::XPath.first(doc.root, "//response/error")
+      raise "Could not reactivate #{bugid}:\n#{doc}" if error
+      $stderr.puts "Reactivated #{bugid}"
     end
 
     STATES = {:fixed => 2, :completed => 15, :implemented => 8}
